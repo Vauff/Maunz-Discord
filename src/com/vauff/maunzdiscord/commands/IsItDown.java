4e5164699d875c411212b40def4ce904ddb030d0
Raqbit <raqbit@outlook.com>
2017-04-03 20:48:16 +0200

    Remove aliases, Correct alias, corrected codestyle.

--- src/com/vauff/maunzdiscord/commands/IsItDown.java
+++ src/com/vauff/maunzdiscord/commands/IsItDown.java
@@ -12,35 +12,47 @@ import java.net.Socket;
 /**
  * Created by Ramon on 03-Apr-17.
  */
-public class IsItDown implements ICommand<MessageReceivedEvent> {
+public class IsItDown implements ICommand<MessageReceivedEvent>
+{
     @Override
-    public void exe(MessageReceivedEvent event) throws Exception {
+    public void exe(MessageReceivedEvent event) throws Exception
+    {
         String[] args = event.getMessage().getContent().split(" ");
 
-        if (args.length == 2) {
+        if (args.length == 2)
+        {
             boolean isUp;
             String hostname = args[2].replaceAll("^https?:\\/\\/", "").split("/")[0];
-            if (args[2].startsWith("https")) {
+            if (args[2].startsWith("https"))
+            {
                 isUp = pingHost(hostname, 443, 4000);
-            } else {
+            }
+            else
+            {
                 isUp = pingHost(hostname, 80, 4000);
             }
             Util.msg(event.getMessage().getChannel(), hostname + " is currently " + (isUp ? "UP" : "DOWN") + ".");
-        } else {
+        }
+        else
+        {
             Util.msg(event.getMessage().getChannel(), "Usage: *isitdown <hostname>");
         }
     }
 
     @Override
-    public String[] getAliases() {
-        return new String[]{"isdown", "isitdown", "isup", "isitup"};
+    public String[] getAliases()
+    {
+        return new String[]{"*isitdown"};
     }
 
-    private static boolean pingHost(String host, int port, int timeout) {
-        try (Socket socket = new Socket()) {
+    private static boolean pingHost(String host, int port, int timeout)
+    {
+        try (Socket socket = new Socket())
+        {
             socket.connect(new InetSocketAddress(host, port), timeout);
             return true;
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             return false; // Either timeout or unreachable or failed DNS lookup.
         }
     }
