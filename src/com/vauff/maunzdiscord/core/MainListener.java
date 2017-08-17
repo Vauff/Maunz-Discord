8892d8139de2ca55cdcc84c576ab730b6fda4151
bl4ckscor3 <donalddani9612@gmx.de>
2017-08-17 21:53:26 +0200

    earlier deletion

--- src/com/vauff/maunzdiscord/core/MainListener.java
+++ src/com/vauff/maunzdiscord/core/MainListener.java
@@ -106,9 +106,9 @@ public class MainListener
 		{
 			if (AbstractCommand.AWAITED.containsKey(event.getMessage().getStringID()) && event.getUser().getStringID().equals(AbstractCommand.AWAITED.get(event.getMessage().getStringID()).getUserID()))
 			{
-				AbstractCommand.AWAITED.get(event.getMessage().getStringID()).getCommand().onReactionAdd(event);
-				AbstractCommand.AWAITED.remove(event.getMessage().getStringID());
 				event.getMessage().delete();
+				AbstractCommand.AWAITED.remove(event.getMessage().getStringID());
+				AbstractCommand.AWAITED.get(event.getMessage().getStringID()).getCommand().onReactionAdd(event);
 			}
 		}
 		catch (Exception e)
