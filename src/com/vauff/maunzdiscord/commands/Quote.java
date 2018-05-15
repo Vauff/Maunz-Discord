cc1967b0f3fc9ef6e4e319609ed276d825edae4e
Vauff <mctehkitti@gmail.com>
2018-05-15 19:31:59 -0400

    Temporarily disable quote command in most guilds

--- src/com/vauff/maunzdiscord/commands/Quote.java
+++ src/com/vauff/maunzdiscord/commands/Quote.java
@@ -20,138 +20,145 @@ public class Quote extends AbstractCommand<MessageReceivedEvent>
 	@Override
 	public void exe(MessageReceivedEvent event) throws Exception
 	{
-		String[] args = event.getMessage().getContent().split(" ");
-
-		if (args.length == 1)
+		if (event.getGuild().getLongID() != 252536814324154368L) // hardcoding is bad mmkay, but this is temporary
 		{
-			Util.msg(event.getChannel(), event.getAuthor(), "You can view the quotes site here: http://158.69.59.239/quotes/");
+			Util.msg(event.getChannel(), event.getAuthor(), "The quote command has been temporarily disabled in most guilds until a rewrite can be completed");
 		}
 		else
 		{
-			switch (args[1].toLowerCase())
+			String[] args = event.getMessage().getContent().split(" ");
+
+			if (args.length == 1)
 			{
-				case "list":
-					if (args.length == 2 || NumberUtils.isCreatable(args[2]))
-					{
-						int page;
-
-						if (args.length == 2)
+				Util.msg(event.getChannel(), event.getAuthor(), "You can view the quotes site here: http://158.69.59.239/quotes/");
+			}
+			else
+			{
+				switch (args[1].toLowerCase())
+				{
+					case "list":
+						if (args.length == 2 || NumberUtils.isCreatable(args[2]))
 						{
-							page = 1;
-						}
-						else
-						{
-							page = Integer.parseInt(args[2]);
-						}
+							int page;
 
-						Util.sqlConnect();
-
-						PreparedStatement pst = Util.sqlCon.prepareStatement("SELECT * FROM quotes;");
-						ResultSet rs = pst.executeQuery();
-
-						ArrayList<String> list = new ArrayList<>();
-
-						while (rs.next())
-						{
-							if (rs.getInt("approved") == 1)
+							if (args.length == 2)
 							{
-								list.add(rs.getString("title"));
-							}
-						}
-
-						IMessage m = Util.buildPage(list, "Quotes List", 10, page, true, true, event.getChannel(), event.getAuthor());
-
-						listMessages.put(event.getAuthor().getStringID(), m.getStringID());
-						waitForReaction(m.getStringID(), event.getAuthor().getStringID());
-						listPages.put(event.getAuthor().getStringID(), page);
-
-						Util.sqlCon.abort(command ->
-						{
-						});
-					}
-					else
-					{
-						Util.msg(event.getChannel(), event.getAuthor(), "Page numbers need to be numerical!");
-					}
-
-					break;
-				case "view":
-					if (args.length == 2)
-					{
-						Util.msg(event.getChannel(), event.getAuthor(), "You need to provide a quote ID! **Usage: \\*quote view <quoteid>**");
-					}
-					else
-					{
-						if (NumberUtils.isCreatable(args[2]))
-						{
-							Util.sqlConnect();
-							PreparedStatement pst = Util.sqlCon.prepareStatement("SELECT * FROM quotes WHERE id='" + args[2] + "'");
-							ResultSet rs = pst.executeQuery();
-
-							if (!rs.next())
-							{
-								Util.msg(event.getChannel(), event.getAuthor(), "That quote doesn't exist!");
+								page = 1;
 							}
 							else
 							{
+								page = Integer.parseInt(args[2]);
+							}
+
+							Util.sqlConnect();
+
+							PreparedStatement pst = Util.sqlCon.prepareStatement("SELECT * FROM quotes;");
+							ResultSet rs = pst.executeQuery();
+
+							ArrayList<String> list = new ArrayList<>();
+
+							while (rs.next())
+							{
 								if (rs.getInt("approved") == 1)
 								{
-									int lines = 0;
-									boolean cut = false;
-									StringBuilder quote = new StringBuilder();
-
-									quote.append("```" + System.lineSeparator());
-									Util.msg(event.getChannel(), event.getAuthor(), "**ID:** " + rs.getString("id") + " **Title:** " + rs.getString("title") + " **Submitter:** " + rs.getString("submitter") + " **Date:** " + Util.getTime(rs.getLong("time") * 1000));
-
-									for (String s : rs.getString("quote").split("\n"))
-									{
-										if (lines < 10)
-										{
-											lines++;
-											quote.append(s + System.lineSeparator());
-										}
-										else
-										{
-											cut = true;
-											break;
-										}
-									}
-
-									quote.append("```");
-									Util.msg(event.getChannel(), event.getAuthor(), quote.toString());
-
-									if (cut)
-									{
-										Util.msg(event.getChannel(), event.getAuthor(), "The rest of this quote is too long for Discord. Please see the full quote at http://158.69.59.239/quotes/viewquote.php?id=" + args[2]);
-									}
-								}
-								else
-								{
-									Util.msg(event.getChannel(), event.getAuthor(), "That quote hasn't been approved yet!");
+									list.add(rs.getString("title"));
 								}
 							}
 
-							rs.close();
-							pst.close();
+							IMessage m = Util.buildPage(list, "Quotes List", 10, page, true, true, event.getChannel(), event.getAuthor());
+
+							listMessages.put(event.getAuthor().getStringID(), m.getStringID());
+							waitForReaction(m.getStringID(), event.getAuthor().getStringID());
+							listPages.put(event.getAuthor().getStringID(), page);
+
 							Util.sqlCon.abort(command ->
 							{
 							});
 						}
 						else
 						{
-							Util.msg(event.getChannel(), event.getAuthor(), "Quote IDs need to be numerical!");
+							Util.msg(event.getChannel(), event.getAuthor(), "Page numbers need to be numerical!");
 						}
-					}
 
-					break;
-				case "add":
-					Util.msg(event.getChannel(), event.getAuthor(), "You can submit new quotes here: http://158.69.59.239/quotes/addquote.php");
+						break;
+					case "view":
+						if (args.length == 2)
+						{
+							Util.msg(event.getChannel(), event.getAuthor(), "You need to provide a quote ID! **Usage: \\*quote view <quoteid>**");
+						}
+						else
+						{
+							if (NumberUtils.isCreatable(args[2]))
+							{
+								Util.sqlConnect();
+								PreparedStatement pst = Util.sqlCon.prepareStatement("SELECT * FROM quotes WHERE id='" + args[2] + "'");
+								ResultSet rs = pst.executeQuery();
 
-					break;
-				default:
-					Util.msg(event.getChannel(), event.getAuthor(), "The argument **" + args[1] + "** was not recognized! See **\\*help quote**");
+								if (!rs.next())
+								{
+									Util.msg(event.getChannel(), event.getAuthor(), "That quote doesn't exist!");
+								}
+								else
+								{
+									if (rs.getInt("approved") == 1)
+									{
+										int lines = 0;
+										boolean cut = false;
+										StringBuilder quote = new StringBuilder();
 
-					break;
+										quote.append("```" + System.lineSeparator());
+										Util.msg(event.getChannel(), event.getAuthor(), "**ID:** " + rs.getString("id") + " **Title:** " + rs.getString("title") + " **Submitter:** " + rs.getString("submitter") + " **Date:** " + Util.getTime(rs.getLong("time") * 1000));
+
+										for (String s : rs.getString("quote").split("\n"))
+										{
+											if (lines < 10)
+											{
+												lines++;
+												quote.append(s + System.lineSeparator());
+											}
+											else
+											{
+												cut = true;
+												break;
+											}
+										}
+
+										quote.append("```");
+										Util.msg(event.getChannel(), event.getAuthor(), quote.toString());
+
+										if (cut)
+										{
+											Util.msg(event.getChannel(), event.getAuthor(), "The rest of this quote is too long for Discord. Please see the full quote at http://158.69.59.239/quotes/viewquote.php?id=" + args[2]);
+										}
+									}
+									else
+									{
+										Util.msg(event.getChannel(), event.getAuthor(), "That quote hasn't been approved yet!");
+									}
+								}
+
+								rs.close();
+								pst.close();
+								Util.sqlCon.abort(command ->
+								{
+								});
+							}
+							else
+							{
+								Util.msg(event.getChannel(), event.getAuthor(), "Quote IDs need to be numerical!");
+							}
+						}
+
+						break;
+					case "add":
+						Util.msg(event.getChannel(), event.getAuthor(), "You can submit new quotes here: http://158.69.59.239/quotes/addquote.php");
+
+						break;
+					default:
+						Util.msg(event.getChannel(), event.getAuthor(), "The argument **" + args[1] + "** was not recognized! See **\\*help quote**");
+
+						break;
+				}
 			}
 		}
 	}
