2be0f3b23dc670ce5aad8ebe34b60d26032d8328
Vauff <mctehkitti@gmail.com>
2020-05-13 22:43:00 -0400

    make *say code more readable, fix possible double message

--- src/com/vauff/maunzdiscord/commands/Say.java
+++ src/com/vauff/maunzdiscord/commands/Say.java
@@ -18,65 +18,63 @@ public class Say extends AbstractCommand<MessageCreateEvent>
 	{
 		String[] args = event.getMessage().getContent().get().split(" ");
 
-		if (!(channel instanceof PrivateChannel))
+		if (channel instanceof PrivateChannel)
 		{
-			if (Util.hasPermission(author, event.getGuild().block()))
+			Util.msg(channel, author, "This command can't be done in a PM, only in a guild in which you have the administrator permission in");
+			return;
+		}
+
+		if (!Util.hasPermission(author, event.getGuild().block()))
+		{
+			Util.msg(channel, author, "You do not have permission to use that command");
+			return;
+		}
+
+		if (args.length == 1)
+		{
+			Util.msg(channel, author, "I need a message to send! **Usage: *say [channel] <message>**");
+			return;
+		}
+
+		if (args[1].startsWith("<#"))
+		{
+			if (args.length == 2)
 			{
-				if (args.length != 1)
-				{
-					if (args[1].startsWith("<#"))
-					{
-						if (args.length != 2)
-						{
-							try
-							{
-								GuildChannel sendChannel = (GuildChannel) Main.client.getChannelById(Snowflake.of(args[1].replaceAll("[^\\d.]", ""))).block();
+				Util.msg(channel, author, "I need a message to send! **Usage: *say [channel] <message>**");
+				return;
+			}
 
-								if (sendChannel.getGuild().block().equals(event.getGuild().block()))
-								{
-									if (Util.msg((MessageChannel) sendChannel, Util.addArgs(args, 2)) != null)
-									{
-										Util.msg(channel, author, "Successfully sent message!");
-									}
-									else
-									{
-										Util.msg(channel, author, "Failed to send message, the bot doesn't have permissions for this channel");
-									}
+			GuildChannel sendChannel;
 
-								}
-								else
-								{
-									Util.msg(channel, author, "Failed to send message, that channel is in another guild!");
-								}
-							}
-							catch (ClientException e)
-							{
-								Util.msg(channel, author, "Failed to send message, that channel is in another guild!");
-							}
-						}
-						else
-						{
-							Util.msg(channel, author, "I need a message to send! **Usage: *say [channel] <message>**");
-						}
-					}
-					else
-					{
-						Util.msg(channel, Util.addArgs(args, 1));
-					}
-				}
-				else
-				{
-					Util.msg(channel, author, "I need a message to send! **Usage: *say [channel] <message>**");
-				}
+			try
+			{
+				sendChannel = (GuildChannel) Main.client.getChannelById(Snowflake.of(args[1].replaceAll("[^\\d.]", ""))).block();
+			}
+			catch (ClientException e)
+			{
+				Util.msg(channel, author, "Failed to send message, that channel is in another guild!");
+				return;
+			}
+
+			if (!sendChannel.getGuild().block().equals(event.getGuild().block()))
+			{
+				Util.msg(channel, author, "Failed to send message, that channel is in another guild!");
+				return;
+			}
+
+			if (Util.msg((MessageChannel) sendChannel, Util.addArgs(args, 2)) != null)
+			{
+				if (!sendChannel.equals(channel))
+					Util.msg(channel, author, "Successfully sent message!");
 			}
 			else
 			{
-				Util.msg(channel, author, "You do not have permission to use that command");
+				Util.msg(channel, author, "Failed to send message, the bot doesn't have permissions for this channel");
 			}
 		}
 		else
 		{
-			Util.msg(channel, author, "This command can't be done in a PM, only in a guild in which you have the administrator permission in");
+			Util.msg(channel, Util.addArgs(args, 1));
 		}
 	}
 
