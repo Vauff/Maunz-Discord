23951757ea8a4d41396c75cc11a8a2710ce1511d
Vauff <mctehkitti@gmail.com>
2022-05-02 23:47:28 -0400

    Add a self-restart if bot has hung

--- src/com/vauff/maunzdiscord/core/Config.java
+++ src/com/vauff/maunzdiscord/core/Config.java
@@ -22,6 +22,7 @@ public class Config extends JSONObject
 			put("botOwners", new JSONArray());
 			put("devGuilds", new JSONArray());
 			put("discordToken", "");
+			put("restartWhenHung", false);
 			put("mongoDatabase", new JSONObject());
 			getJSONObject("mongoDatabase").put("connectionString", "");
 			getJSONObject("mongoDatabase").put("database", "");
@@ -49,6 +50,11 @@ public class Config extends JSONObject
 	public String getToken()
 	{
 		return getString("discordToken");
+	}
+
+	public boolean isHangRestartEnabled()
+	{
+		return getBoolean("restartWhenHung");
 	}
 
 	public String getConnectionString()
---
+++ src/com/vauff/maunzdiscord/timers/HangTimer.java
@@ -0,0 +1,44 @@
+package com.vauff.maunzdiscord.timers;
+
+import com.vauff.maunzdiscord.core.Logger;
+import com.vauff.maunzdiscord.core.Main;
+
+import java.time.Instant;
+
+/**
+ * Shitty workaround for Discord4J hanging when reconnecting to Discord, force restarts the bot if no progress is being made after 15 minutes
+ *
+ * Note: This requires some sort of external restarter (e.g. being registered as a systemd service) to take care of starting the bot back up again, so this is disabled by default (see "" in config.json)
+ */
+public class HangTimer
+{
+	private static Long disconnectedAt = 0L;
+
+	public static Runnable timer = () ->
+	{
+		try
+		{
+			if (Main.gateway.getGatewayClient(0).get().isConnected().block())
+			{
+				disconnectedAt = 0L;
+			}
+			else
+			{
+				long now = Instant.now().getEpochSecond();
+
+				if (disconnectedAt == 0L)
+					disconnectedAt = now;
+
+				if ((disconnectedAt + 900L) < now)
+				{
+					Logger.log.fatal("Detected bot in hung state, attempting to force a restart");
+					System.exit(1);
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			Logger.log.error("", e);
+		}
+	};
+}
