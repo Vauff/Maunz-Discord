4a4e728ee0aec1d2b2153ed9306b9c951dfc407b
Vauff <mctehkitti@gmail.com>
2022-05-05 18:01:12 -0400

    Bump version

--- src/com/vauff/maunzdiscord/core/Main.java
+++ src/com/vauff/maunzdiscord/core/Main.java
@@ -38,7 +38,7 @@ public class Main
 {
 	public static GatewayDiscordClient gateway;
 	public static MongoDatabase mongoDatabase;
-	public static String version = "r46";
+	public static String version = "r47";
 	public static Config cfg;
 
 	/**
