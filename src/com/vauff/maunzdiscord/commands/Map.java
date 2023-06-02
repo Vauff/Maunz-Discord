bfa4d9289eaf21a8fdb19c028d40513e1b6d9b3b
Vauff <mctehkitti@gmail.com>
2023-06-02 09:47:21 -0400

    We can use https now
    Previously cared about displaying less characters (http redirects to https)

--- src/com/vauff/maunzdiscord/commands/Map.java
+++ src/com/vauff/maunzdiscord/commands/Map.java
@@ -133,7 +133,7 @@ public class Map extends AbstractCommand<ChatInputInteractionEvent>
 			EmbedCreateSpec embed = EmbedCreateSpec.builder()
 				.color(MapImages.getMapImageColour(url))
 				.timestamp(Instant.ofEpochMilli(serverDoc.getLong("timestamp")))
-				.description("Currently Playing: **" + doc.getString("lastMap").replace("_", "\\_") + "**\nPlayers Online: **" + serverDoc.getString("playerCount") + "**\nQuick Join: **[" + ipPort + "](http://vauff.com/?ip=" + ipPort + ")**")
+				.description("Currently Playing: **" + doc.getString("lastMap").replace("_", "\\_") + "**\nPlayers Online: **" + serverDoc.getString("playerCount") + "**\nQuick Join: **[" + ipPort + "](https://vauff.com/?ip=" + ipPort + ")**")
 				.build();
 
 			if (!url.equals(""))
